1b5ca4c2d09c427679dd6045125f030bdf216193
Lopez Nechiza <aglopezn@indracompany.com>
2020-12-23 16:25:15 -0500

    Front: Path Contente component added.

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PathComponent } from './components/path/path.component';
 import { OptionsNavComponent } from './components/options-nav/options-nav.component';
+import { PathContentComponent } from './path-content/path-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PathComponent,
-    OptionsNavComponent
+    OptionsNavComponent,
+    PathContentComponent
   ],
   imports: [
     BrowserModule
