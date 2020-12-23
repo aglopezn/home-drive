239b85b8f5b67ff4191a152bd56ae5792c123546
Lopez Nechiza <aglopezn@indracompany.com>
2020-12-23 11:30:19 -0500

    Front: path component created.

---
+++ front/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {} from './components/path/path.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'front';
+}
